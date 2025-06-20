3ddc5f14e46b4c04dcaf5cdce53a4f67d48dcbae
Sonoshi <Cheesecake_Ninja@hotmail.com>
2025-06-21 00:04:41 +0900

    metricool didnt like the script embedded. moved to index.html test2

--- src/components/NewsSection.tsx
+++ src/components/NewsSection.tsx
@@ -1,22 +1,7 @@
-// src/components/NewsSection.tsx
-import { useEffect } from 'react';
-
 export default function NewsSection() {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://tracker.metricool.com/resources/be.js';
-        script.async = true;
-        script.onload = () => {
-            if ((window as any).beTracker) {
-                (window as any).beTracker.t({ hash: '9f3a7e13ce61cd3d106c065e58cd3b81' });
-            }
-        };
-        document.head.appendChild(script);
-    }, []);
-
     return (
         <section className="section">
-            <h2 className="section-title"></h2>
+            <h2 className="section-title">News & Updates</h2>
             <div id="be-tracker-container"></div>
         </section>
     );
