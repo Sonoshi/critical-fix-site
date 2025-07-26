9fcc57589ef910c4fe05d746b1d4b88ccc62caea
Sonoshi <Cheesecake_Ninja@hotmail.com>
2025-07-26 16:37:43 +0900

    making sure pnp is working, didnt save

--- src/pages/playPage.tsx
+++ src/pages/playPage.tsx
@@ -71,8 +71,8 @@ export default function PlayPage() {
 
     // Automatically trigger file download
     const link = document.createElement("a");
-    link.href = "/assets/Critical_Fix_PnP.zip";
-    link.download = "Critical_Fix_PnP.zip";
+    link.href = "/assets/Critical_Fix_PnP_v2.zip";
+    link.download = "Critical_Fix_PnP_v2.zip";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -151,7 +151,7 @@ export default function PlayPage() {
                 You're in! Here's your download:
               </p>
               <a
-                href="/assets/Critical_Fix_PnP.zip"
+                href="/assets/Critical_Fix_PnP_v2.zip"
                 download
                 className="download-button"
                 onClick={sendDiscordMessage}
