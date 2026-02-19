a09a506bd41da475c4ac87000d64fc493003283d
Kyle <kyle.petruska@gmail.com>
2026-02-19 23:20:50 +0900

    One last Carousel adjustment...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // keep this as "/" for root domain like critical-fix.com
-})
+  base: "/", // keep this as "/" for root domain like critical-fix.com
+  server: { host: true },
+});
